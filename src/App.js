a81d2f565a3c3f0c8bcbf4faa7302453d3924da1
marcosfilhobr84 <107562601+marcosfilhobr84@users.noreply.github.com>
2023-06-07 19:41:33 -0300

    adiciona roteamento

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
 import Home from "./components/pages/Home";
-import "./styles/normalize.css";
-import "./styles/fontawesome.min.css";
-import "./styles/main.css";
 import Users from "./components/pages/Users";
 import UserBlog from "./components/pages/UserBlog";
 import UserPostForm from "./components/pages/UserPostForm";
 
+import "./styles/normalize.css";
+import "./styles/fontawesome.min.css";
+import "./styles/main.css";
+
 function App() {
-  return <UserPostForm />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/users" element={<Users />}></Route>
+        <Route path="/users/:UserId" element={<UserBlog />}></Route>
+        <Route path="/users/:UserId/post" element={<UserPostForm />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
